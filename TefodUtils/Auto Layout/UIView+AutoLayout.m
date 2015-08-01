5f6d326f7c5f7e2fc30f35b5c5c83ff05d82f4f2
Eric Chai <flig@me.com>
2015-08-01 22:36:52 +0800

    Auto Layout: 增加了汇集 constraints 的 UIView 扩展。

--- TefodUtils/Auto Layout/UIView+AutoLayout.m
+++ TefodUtils/Auto Layout/UIView+AutoLayout.m
@@ -153,4 +153,32 @@
 	}
 }
 
+- (NSArray *)autoLayoutConstraintsWithVF:(id)object views:(NSDictionary *)views metrics:(NSDictionary *)metrics {
+	NSMutableArray *constraints = [NSMutableArray array];
+	
+	if (!object)
+		return constraints;
+	
+	if ([object isKindOfClass:[NSString class]]) {
+		// 如果是一个字符串，直接把它当做一个布局表达式
+		[constraints addObjectsFromArray:[NSLayoutConstraint
+										  constraintsWithVisualFormat:object options:0
+										  metrics:metrics views:views]];
+		
+	} else if ([object isKindOfClass:[NSArray class]]) {
+		// 如果是一个数组，把它当做一系列布局表达式的集合
+		for (NSString *vf in object) {
+			[constraints addObjectsFromArray:[NSLayoutConstraint
+											  constraintsWithVisualFormat:vf options:0
+											  metrics:metrics views:views]];
+		}
+		
+	} else {
+		// 非法的数据类型
+		return constraints;
+	}
+	
+	return constraints;
+}
+
 @end
